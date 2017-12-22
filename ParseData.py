eabe804d27324a00279f1564b6e00374e43ffeaf
Alex Simon <ajsimon@mtu.edu>
2017-12-21 19:50:33 -0600

    Adding files to support training a neural network

---
+++ ParseData.py
@@ -0,0 +1,25 @@
+import csv
+import numpy as np
+import os
+
+def GetTrainingData():
+    data_list = []
+    
+    with open(os.path.dirname(os.path.abspath(__file__)) + '\Replays\\replay.csv', 'rt') as fi:
+        reader = csv.reader(fi)
+        for row in reader:
+            #skip the header
+            if row[0] != 'in0':
+                data_list.append(list(map(float, row)))
+
+    return np.matrix(data_list)
+
+"""
+data = GetTrainingData()
+input_data = data[:, :27]
+output_data = data[:, 28:]
+
+print(data[0])
+print(input_data[0])
+print(output_data[0])
+"""
