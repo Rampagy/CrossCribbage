32533824314b3c20b6c780ba8dfb5f9403725d72
Alex Simon <ajsimon@mtu.edu>
2017-12-21 20:10:35 -0600

    Changed sizes to try and fix bug

--- TensorBot.py
+++ TensorBot.py
@@ -36,8 +36,8 @@ def cnn_model_fn(features, labels, mode):
       
   # Logits layer
   # Input Tensor Shape: [batch_size, 512]
-  # Output Tensor Shape: [batch_size, 26]
-  logits = tf.layers.dense(inputs=dropout2, units=26)
+  # Output Tensor Shape: [batch_size, 1]
+  logits = tf.layers.dense(inputs=dropout2, units=1)
 
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
@@ -50,7 +50,9 @@ def cnn_model_fn(features, labels, mode):
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
   # Calculate Loss (for both TRAIN and EVAL modes)
-  onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=26)
+  onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=1)
+  print(onehot_labels)
+  print(logits)
   loss = tf.losses.softmax_cross_entropy(onehot_labels=onehot_labels, logits=logits)
 
   # Configure the Training Op (for TRAIN mode)
@@ -81,7 +83,7 @@ def main(unused_argv):
   train_labels = data[:, 27:]
   
   # Create the Estimator
-  mnist_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
+  cribbage_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="/tmp/mnist_convnet_model")
 
   # Set up logging for predictions
   # Log the values in the "Softmax" tensor with label "probabilities"
@@ -95,7 +97,7 @@ def main(unused_argv):
     batch_size=100,
     num_epochs=None,
     shuffle=True)
-  mnist_classifier.train(input_fn=train_input_fn,
+  cribbage_classifier.train(input_fn=train_input_fn,
     steps=20000,
     hooks=[logging_hook])
 
@@ -106,7 +108,7 @@ def main(unused_argv):
     num_epochs=1,
     shuffle=False)
     
-  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  eval_results = cribbage_classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
 if __name__ == "__main__":
